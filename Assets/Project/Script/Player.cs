d3f23e7a24a6420a36e6e794e6cba0fbcae56bfa
carrotcookie <a44121078@gmail.com>
2024-05-07 21:23:03 +0900

    오류수정
    1. 움직이고나서 바라보는 방향 전환
    2. Dynamic 가시 장애물과 충돌버그가 있었지만
       코드를 건든게 없는데 갑자기 사라짐...

--- Assets/Project/Script/Player.cs
+++ Assets/Project/Script/Player.cs
@@ -2,18 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour {
     [SerializeField] Tilemap tilemap;
+    SpriteRenderer spriter;
     bool isMove = false;
-    float xInput;
-    float yInput;
+    Vector2 inputVec = Vector2.zero;
 
-    void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.CompareTag("Thorn")) {
-            Debug.Log("Player 사망");
-            gameObject.SetActive(false);
-        }
+    void Awake() {
+        spriter = GetComponent<SpriteRenderer>();
     }
 
     void Update() {
@@ -21,39 +19,79 @@ public class Player : MonoBehaviour {
         Move();
     }
 
-    Vector3 GetNextPosition(Vector2 dirVec) {
-        RaycastHit2D hit1 = Physics2D.Raycast(transform.position, dirVec, Mathf.Infinity, LayerMask.GetMask("Obstacle"));
-        if (hit1.collider != null)
-            Debug.Log(hit1.transform.name);
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Thorn")) {
+            // 사망 처리
+            Debug.Log("가시에 질려 사망하였습니다.");
+        }
+    }
 
+    void GetInput() {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void Move() {
+        if (isMove)
+            return;
+        if (inputVec == Vector2.zero || inputVec == Vector2.one)
+            return;
+
+
+        isMove = true;
+        StartCoroutine(MoveTo(inputVec));
+    }
+
+    void Stop(Vector2 dirVec) {
+        Rotate(dirVec);
+        isMove = false;
+
+        // 완전히 이동하고 나서 장애물 체크를 실행
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dirVec, Mathf.Infinity, LayerMask.GetMask("Obstacle"));
+        if (!hit)
+            return;
+
+        
+
+        if (hit.collider.CompareTag("Thorn")) {
+            // 사망 처리
+            Debug.Log("가시에 질려 사망하였습니다.");
+        }
+    }
+
+    void Rotate(Vector2 dirVec) {
+        Vector3 rotVec = Vector3.zero;
+
+        if (dirVec.x != 0) {
+            if (dirVec.x == 1)
+                rotVec = new Vector3(0, 0, 90);
+            else
+                rotVec = new Vector3(0, 0, -90);
+        }
+        else if (dirVec.y != 0) {
+            if (dirVec.y == 1)
+                rotVec = new Vector3(0, 0, 180);
+            else
+                rotVec = new Vector3(0, 0, 0);
+        }
+
+        transform.rotation = Quaternion.Euler(rotVec);
+        spriter.flipX = Random.Range(0, 2) == 0 ? false : true;
+    }
+
+    Vector3 GetNextPosition(Vector2 dirVec) {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, dirVec, Mathf.Infinity, LayerMask.GetMask("Tilemap"));
         Vector3Int cellPos = tilemap.WorldToCell(hit.point);
         Vector3 nextPos = tilemap.GetCellCenterWorld(cellPos);
         return nextPos;
     }
 
-    void GetInput() {
-        xInput = Input.GetAxisRaw("Horizontal");
-        yInput = Input.GetAxisRaw("Vertical");
-    }
-
-    void Move() {
-        if (isMove)
-            return;
-        if (xInput == 0 && yInput == 0)
-            return;
-
-
-        isMove = true;
-        Vector2 dirVec = new Vector2(xInput, yInput);
-        Vector3 endPos = GetNextPosition(dirVec);
-        StartCoroutine(MoveTo(endPos));
-    }
-
-    IEnumerator MoveTo(Vector2 endPos) {
+    IEnumerator MoveTo(Vector2 dirVec) {
         Vector3 startPos = transform.position;
+        Vector3 endPos = GetNextPosition(dirVec);
+
         float timer = 0;
-        float duration = 0.05f * Vector3.Distance(startPos, endPos);
+        float duration = 0.05f * Vector3.Distance(startPos, endPos); // 속도가 일정한 상태에서 거리가 늘면 duration도 늘어남
 
         while (true) {
             timer += Time.deltaTime / duration;
@@ -61,9 +99,8 @@ public class Player : MonoBehaviour {
             if (timer < 1f)
                 transform.position = Vector2.Lerp(startPos, endPos, timer);
             else {
-                timer = 1f;
                 transform.position = endPos;
-                isMove = false;
+                Stop(dirVec);
                 break;
             }
 
